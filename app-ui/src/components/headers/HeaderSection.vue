6a9a48c9194e89ceb75b5595da4f7dddba6cc226
Isaiah Velarde <isaiah.velarde01@gmail.com>
2024-08-02 16:50:56 -0500

    Changed Header Spelling

--- app-ui/src/components/headers/HeaderSection.vue
+++ app-ui/src/components/headers/HeaderSection.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-white shadow-md p-4">
     <div class="header-container mx-auto">
-      <h1 class="text-2xl font-bold">Neurocorp Therapy Center</h1>
+      <h1 class="text-2xl font-bold">Neurocorp Therapy Centre</h1>
     </div>
   </header>
 </template>
